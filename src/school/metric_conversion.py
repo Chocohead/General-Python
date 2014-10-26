21642cf078e03c9ad17422643d9a0792f527a600
satanicsanta <elifosterwy@gmail.com>
2014-10-26 00:13:26 -0700

    Commit

--- src/school/metric_conversion.py
+++ src/school/metric_conversion.py
@@ -1,9 +1,45 @@
 __author__ = 'elijahfoster-wysocki'
 
+length=0
+volume=1
+mass=2
+
 def length():
-    base = input(float("What is the base unit?\n"))
+    base = float(input("What is the base unit?\n"))
     print("What conversion?\n")
     conversion = input("Enter: kilo for meter -> kilometer, centi for meter -> centimeter, or milli for meter -> millimeter\n")
+
+    if conversion == str("kilo"):
+        print str(base * 1000)
+
+    if conversion == str("centi"):
+        print str(base * 0.01)
+
+    if conversion == str("milli"):
+        print str(base * 0.001)
+
+    return
+
+def volume():
+    base = float(input("What is the base unit?\n"))
+    print("What conversion?\n")
+    conversion = input("Enter: kilo for liter -> kiloliter, centi for liter -> centiliter, or milli for liter -> milliliter\n")
+
+    if conversion == str("kilo"):
+        print str(base * 1000)
+
+    if conversion == str("centi"):
+        print str(base * 0.01)
+
+    if conversion == str("milli"):
+        print str(base * 0.001)
+
+    return
+
+def mass():
+    base = float(input("What is the base unit?\n"))
+    print("What conversion?\n")
+    conversion = input("Enter: kilo for gram -> kilogram, centi for gram -> centigram, or milli for gram -> milligram\n")
 
     if conversion == str("kilo"):
         print str(base * 1000)
@@ -19,12 +55,17 @@ def length():
 def perform():
     print("Welcome to the Metric Conversion Calculator.\n")
     print("What property would you like to convert?\n")
-    question = input(str("Enter: length for length, volume for volume, or mass for mass\n"))
-    if question == "length":
+    question = int(input("Enter: 0 for length, 1 for volume, or 2 for mass\n"))
+    if question == length:
         length()
+    elif question == volume:
+        volume()
+    elif question == mass:
+        mass()
+    else:
+        print("nigga.")
 
-
-
+    return
 
 
 perform()
