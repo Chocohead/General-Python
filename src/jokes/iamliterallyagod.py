f37915335a6cf73806df4e303622bfedb0ac037b
satanicsanta <elifosterwy@gmail.com>
2014-12-01 22:48:49 -0800

    I AM A GOD.

---
+++ src/jokes/iamliterallyagod.py
@@ -0,0 +1,12 @@
+# Created by Eli Foster
+
+import time
+import sys
+
+x = input("Input a number: ")
+time.sleep(15)
+print("Okay. You are still a faggot though.")
+time.sleep(2)
+print("I AM A FUCKING GOD AT PROGRAMMING. I CAN DO LITERALLY ANYTHING YOU FUCKER.")
+
+sys.exit()
